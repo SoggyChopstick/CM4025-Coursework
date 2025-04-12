a15e484302417d9acf54352811c090801dd3c0fe
SoggyChopstick <lukechsmith@icloud.com>
2025-04-12 18:04:21 +0100

    updated with node modules installed

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const http = require('http');
-const fs = require('fs');
+//const http = require('http');
+//const fs = require('fs');
 const path = require('path');
+const express = require('express');
+//const session = require('express-session');
 
-const hostname = '127.0.0.1';
+const app = express();
+
+//const hostname = '127.0.0.1';
 const port = 8080;
 
+app.set('view engine', 'ejs');
+app.use(express.static('Frontend'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+	res.render('pages/myWebPage.ejs')
+});
+
+app.listen(port, () => {
+	console.log('Server running on http://localhost:${port}')
+});
+/*
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'myWebPage.html'), (err, data) => {
+        fs.readFile(path.join(__dirname, '/pages/myWebPage.ejs'), (err, data) => {
             if (err) {
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Internal Server Error');
@@ -25,3 +41,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
